b8843053c5ff0a861c99b28141a8b36c2828853a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-29 10:33:35 +0000

    Deploy dnivra26/mapops to github.com/dnivra26/mapops.git:gh-pages

---
+++ precache-manifest.18e8f65f046df2de6e0b682a30a2d6c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ec4bf522900f7de937e",
+    "url": "/mapops/static/css/main.1149ecbb.chunk.css"
+  },
+  {
+    "revision": "2ec4bf522900f7de937e",
+    "url": "/mapops/static/js/main.2ec4bf52.chunk.js"
+  },
+  {
+    "revision": "c9bbe87529556649fa50",
+    "url": "/mapops/static/js/1.c9bbe875.chunk.js"
+  },
+  {
+    "revision": "1e3d6d901e7f0b4ba65e",
+    "url": "/mapops/static/js/runtime~main.1e3d6d90.js"
+  },
+  {
+    "revision": "d76144990a5626d549ab2c4c7c892f1d",
+    "url": "/mapops/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/mapops/precache-manifest.18e8f65f046df2de6e0b682a30a2d6c3.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mapops/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
